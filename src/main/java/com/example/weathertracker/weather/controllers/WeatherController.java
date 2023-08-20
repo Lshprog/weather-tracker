0cfeb358eada05a4c4c10ee4ab04485e56aefe85
Lshprog <onisalex1@gmail.com>
2023-08-21 00:32:07 +0400

    main page

--- src/main/java/com/example/weathertracker/weather/controllers/WeatherController.java
+++ src/main/java/com/example/weathertracker/weather/controllers/WeatherController.java
@@ -1,15 +1,26 @@
 package com.example.weathertracker.weather.controllers;
 
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.WebRequest;
 
 @Controller
 @RequestMapping("weather")
 public class WeatherController {
 
     @GetMapping("/main_page")
-    public String showMainPage(){
+    public String showMainPage(WebRequest webRequest, Model theModel){
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String username = authentication.getName();
+
+        theModel.addAttribute("username", username);
+
         return "weather/main_page";
     }
 
