b56e685fc68f66f1de580830dcc15da20243aeae
Lshprog <onisalex1@gmail.com>
2023-08-23 19:29:44 +0400

    Implemented function to retrieve json with forecast

--- src/main/java/com/example/weathertracker/apiexternal/services/WeatherAPIService.java
+++ src/main/java/com/example/weathertracker/apiexternal/services/WeatherAPIService.java
@@ -1,17 +1,13 @@
 package com.example.weathertracker.apiexternal.services;
 
+import com.example.weathertracker.apiexternal.common.HourlyForecast;
 import com.example.weathertracker.apiexternal.common.WeatherConditions;
 import com.example.weathertracker.weather.dto.LocationDTO;
-import com.example.weathertracker.weather.entities.Location;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-
-import java.io.IOException;
 
 @Service
 public class WeatherAPIService {
@@ -46,7 +42,22 @@ public class WeatherAPIService {
         )
                 .retrieve()
                 .bodyToFlux(String.class)
-                .map(weatherDeserializationService::deserializeAndExtractWeatherConditions);
+                .map(weatherDeserializationService::deserializeAndExtractCurrentWeatherConditions);
+
+    }
+
+    public Flux<HourlyForecast> getForecastFromLocation(LocationDTO location) {
+
+        return webClient.get().uri(uriBuilder -> uriBuilder
+                        .path("forecast.json")
+                        .queryParam("key", apitoken)
+                        .queryParam("q", location.getLat() + "," + location.getLon())
+                        .queryParam("days", 3)
+                        .build()
+                )
+                .retrieve()
+                .bodyToFlux(String.class)
+                .map(weatherDeserializationService::deserializeAndExtractHourlyForecast);
 
     }
 
