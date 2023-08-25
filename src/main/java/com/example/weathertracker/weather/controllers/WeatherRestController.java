78cda0465378e6fe4510e0ae9ab3b5d52e1f8bf4
Lshprog <onisalex1@gmail.com>
2023-08-25 18:25:54 +0400

    Added rest controller to handle some requests from main page

--- src/main/java/com/example/weathertracker/weather/controllers/WeatherRestController.java
+++ src/main/java/com/example/weathertracker/weather/controllers/WeatherRestController.java
@@ -1,2 +1,28 @@
-package com.example.weathertracker.weather.controllers;public class WeatherRestController {
+package com.example.weathertracker.weather.controllers;
+
+import com.example.weathertracker.apiexternal.services.CountrySearchAPIService;
+import com.example.weathertracker.weather.dto.LocationDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("weather/main_page")
+public class WeatherRestController {
+
+    private final CountrySearchAPIService countrySearchAPIService;
+
+    @Autowired
+    public WeatherRestController(CountrySearchAPIService countrySearchAPIService) {
+        this.countrySearchAPIService = countrySearchAPIService;
+    }
+
+    @GetMapping("/citysuggestions")
+    public Flux<LocationDTO> getCitySuggestions(@RequestParam String query) {
+        return countrySearchAPIService.getLocationsBasedOnInput(query);
+    }
+
 }
