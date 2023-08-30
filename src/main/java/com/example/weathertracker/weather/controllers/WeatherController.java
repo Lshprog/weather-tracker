c72c9e2c6f062eeb1daa55b091c421d989272259
Lshprog <onisalex1@gmail.com>
2023-08-31 02:02:17 +0400

    Handle logic of locations different requests to main page

--- src/main/java/com/example/weathertracker/weather/controllers/WeatherController.java
+++ src/main/java/com/example/weathertracker/weather/controllers/WeatherController.java
@@ -1,6 +1,9 @@
 package com.example.weathertracker.weather.controllers;
 
+import com.example.weathertracker.auth.services.IUserService;
+import com.example.weathertracker.weather.services.ILocationService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -13,13 +16,25 @@ import org.springframework.web.context.request.WebRequest;
 @RequestMapping("weather")
 public class WeatherController {
 
+    private final ILocationService locationService;
+    private final IUserService userService;
+
+    @Autowired
+    public WeatherController(ILocationService locationService, IUserService userService) {
+        this.locationService = locationService;
+        this.userService = userService;
+    }
+
+
     @GetMapping("/main_page")
     public String showMainPage(WebRequest webRequest, Model theModel){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String username = authentication.getName();
 
+
         theModel.addAttribute("username", username);
+        theModel.addAttribute("userLocations", userService.getLocationsForUser(username));
 
         return "weather/main_page";
     }
--- src/main/java/com/example/weathertracker/weather/controllers/WeatherRestController.java
+++ src/main/java/com/example/weathertracker/weather/controllers/WeatherRestController.java
@@ -1,13 +1,20 @@
 package com.example.weathertracker.weather.controllers;
 
 import com.example.weathertracker.apiexternal.services.CountrySearchAPIService;
+import com.example.weathertracker.auth.dto.UserDTO;
+import com.example.weathertracker.auth.entities.User;
+import com.example.weathertracker.auth.services.IUserService;
 import com.example.weathertracker.weather.dto.LocationDTO;
+import com.example.weathertracker.weather.entities.Location;
+import com.example.weathertracker.weather.services.LocationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
 import reactor.core.publisher.Flux;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("weather/main_page")
@@ -15,14 +22,44 @@ public class WeatherRestController {
 
     private final CountrySearchAPIService countrySearchAPIService;
 
+    private final LocationService locationService;
+
+    private final IUserService userService;
+
     @Autowired
-    public WeatherRestController(CountrySearchAPIService countrySearchAPIService) {
+    public WeatherRestController(CountrySearchAPIService countrySearchAPIService, LocationService locationService, IUserService userService) {
         this.countrySearchAPIService = countrySearchAPIService;
+        this.locationService = locationService;
+        this.userService = userService;
     }
 
     @GetMapping("/citysuggestions")
     public Flux<LocationDTO> getCitySuggestions(@RequestParam String query) {
         return countrySearchAPIService.getLocationsBasedOnInput(query);
+    }
+
+    // Need to make no refresh page because, need to go to database again
+    @PostMapping("/addlocation")
+    @ResponseBody
+    public LocationDTO addNewLocation(@RequestBody LocationDTO new_loc){
+
+        //Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        Optional<LocationDTO> location = locationService.getLocationByLatitudeAndLongitude(new_loc.getLat(),
+                new_loc.getLon());
+
+        if(location.isEmpty()){
+            locationService.saveNewLocation(new_loc);
+
+        }
+        else{
+
+        }
+
+        // need to add the new location to user_location table ...
+
+        // change here based on exception
+        return new_loc;
     }
 
 }
